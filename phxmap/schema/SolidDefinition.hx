1757c286e14a52226b1656ede41951f27d07e618
progsy <213481401+progsy@users.noreply.github.com>
2026-07-27 13:52:35 +0200

    Use raw primitives to represent geometry

--- phxmap/schema/SolidDefinition.hx
+++ phxmap/schema/SolidDefinition.hx
@@ -2,11 +2,12 @@ package phxmap.schema;
 
 typedef Geometry = {
 	#if heaps
-	vertices:Array<h3d.Vector>, uvs:Array<h3d.prim.UV>, normals:Array<h3d.Vector>, tangents:Array<h3d.Vector>, indices:Array<hxd.impl.UInt16>
+	positions:Array<hxd.impl.Float32>, uvs:Array<hxd.impl.Float32>, normals:Array<hxd.impl.Float32>, tangents:Array<hxd.impl.Float32>,
+	indices:Array<hxd.impl.UInt16>
 	#elseif kha
-	vertices:Array<kha.FastFloat>, uvs:Array<kha.FastFloat>, normals:Array<kha.FastFloat>, tangents:Array<kha.FastFloat>, indices:Array<Int>
+	positions:Array<kha.FastFloat>, uvs:Array<kha.FastFloat>, normals:Array<kha.FastFloat>, tangents:Array<kha.FastFloat>, indices:Array<Int>
 	#else
-	vertices:Array<Float>, uvs:Array<Float>, normals:Array<Float>, tangents:Array<Float>, indices:Array<Int>
+	positions:Array<Float>, uvs:Array<Float>, normals:Array<Float>, tangents:Array<Float>, indices:Array<Int>
 	#end
 };
 
@@ -31,6 +32,14 @@ class SolidDefinition implements Definition {
 
 	public function load(mapData:phxmap.MapData, index:Int) {
 		var indexOffsets:Map<Geometry, Int> = [];
+		for (geometry in geometries) {
+			geometry.positions.resize(0);
+			geometry.normals.resize(0);
+			geometry.tangents.resize(0);
+			geometry.uvs.resize(0);
+			geometry.indices.resize(0);
+			indexOffsets.set(geometry, 0);
+		}
 		var entity = mapData.entities[index];
 		var i = index;
 		for (j in 0...entity.brushes.length) {
@@ -42,11 +51,11 @@ class SolidDefinition implements Definition {
 					var geometry:Geometry = geometries.get(tag);
 					if (geometry == null) {
 						geometry = {
-							vertices: [],
-							indices: [],
-							uvs: [],
+							positions: [],
 							normals: [],
-							tangents: []
+							tangents: [],
+							uvs: [],
+							indices: []
 						};
 						geometries.set(tag, geometry);
 						indexOffsets.set(geometry, 0);
@@ -54,28 +63,20 @@ class SolidDefinition implements Definition {
 
 					var indexOffset = indexOffsets.get(geometry);
 					for (v in geo.vertices) {
-						#if heaps
-						geometry.vertices.push(new h3d.Vector(Settings.scale(v.vertex.x) - x, Settings.scaleInverse(v.vertex.y) - y,
-							Settings.scale(v.vertex.z) - z));
-						geometry.normals.push(new h3d.Vector(Settings.scale(v.normal.x), Settings.scaleInverse(v.normal.y), Settings.scale(v.normal.z)));
-						geometry.tangents.push(new h3d.Vector(Settings.scale(v.tangent.x), Settings.scaleInverse(v.tangent.y), Settings.scale(v.tangent.z)));
-						geometry.uvs.push(new h3d.prim.UV(v.uv.u, v.uv.v));
-						#else
-						geometry.vertices.push(Settings.scale(v.vertex.x) - x);
-						geometry.vertices.push(#if phxmap.lefthanded Settings.scaleInverse(v.vertex.y) #else Settings.scale(v.vertex.y) #end - y);
-						geometry.vertices.push(Settings.scale(v.vertex.z) - z);
-						geometry.uvs.push(v.uv.u);
-						geometry.uvs.push(v.uv.v);
+						geometry.positions.push(Settings.scale(v.vertex.x) - x);
+						geometry.positions.push(#if (heaps || phxmap.lefthanded) Settings.scaleInverse(v.vertex.y) #else Settings.scale(v.vertex.y) #end - y);
+						geometry.positions.push(Settings.scale(v.vertex.z) - z);
 						geometry.normals.push(v.normal.x);
-						geometry.normals.push(#if phxmap.lefthanded - v.normal.y #else v.normal.y #end);
+						geometry.normals.push(#if (heaps || phxmap.lefthanded) -v.normal.y #else v.normal.y #end);
 						geometry.normals.push(v.normal.z);
 						geometry.tangents.push(v.tangent.x);
-						geometry.tangents.push(#if phxmap.lefthanded - v.tangent.y #else v.tangent.y #end);
+						geometry.tangents.push(#if (heaps || phxmap.lefthanded) -v.tangent.y #else v.tangent.y #end);
 						geometry.tangents.push(v.tangent.z);
-						#if !phxmap.no_tangent_w
-						geometry.tangents.push(#if phxmap.lefthanded - v.tangent.w #else v.tangent.w #end);
+						#if phxmap.tangent_w
+						geometry.tangents.push(#if (heaps || phxmap.lefthanded) -v.tangent.w #else v.tangent.w #end);
 						#end
-						#end
+						geometry.uvs.push(v.uv.u);
+						geometry.uvs.push(v.uv.v);
 					}
 
 					var u = 0;
@@ -99,21 +100,74 @@ class SolidDefinition implements Definition {
 	}
 
 	#if heaps
-	public var primitiveCache:Map<String, h3d.prim.Polygon> = [];
+	public var primitiveCache:Map<String, h3d.prim.RawPrimitive> = [];
 	public var colliderCache:Map<String, h3d.col.Polygon> = [];
 
-	public function getPrimitive(tag:String = DEFAULT_TAG):Null<h3d.prim.Polygon> {
+	public function getPrimitive(tag:String = DEFAULT_TAG):Null<h3d.prim.RawPrimitive> {
 		var primitive = primitiveCache.get(tag);
 		if (primitive == null) {
 			var geometry = geometries.get(tag);
-			if (geometry == null || geometry.vertices.length == 0) {
+			if (geometry == null || geometry.positions.length == 0) {
 				return primitive;
 			}
 
-			primitive = new h3d.prim.Polygon(geometry.vertices, cast geometry.indices);
-			primitive.uvs = geometry.uvs;
-			primitive.normals = geometry.normals;
-			primitive.tangents = geometry.tangents;
+			var format = @:privateAccess new hxd.BufferFormat([
+				{name: 'position', type: DVec3, precision: F32},
+				{name: 'normal', type: DVec3, precision: F32},
+				{name: 'tangent', type: DVec3, precision: F32},
+				{name: 'uv', type: DVec2, precision: F16}
+			]);
+			var cluster = new hxd.FloatBuffer(geometry.positions.length + geometry.normals.length + geometry.tangents.length + geometry.uvs.length);
+			var i = 0;
+			var j = 0;
+			var k = 0;
+			var minX = 0.0;
+			var minY = 0.0;
+			var minZ = 0.0;
+			var maxX = 0.0;
+			var maxY = 0.0;
+			var maxZ = 0.0;
+			while (i < cluster.length) {
+				if (geometry.positions[j] < minX) {
+					minX = geometry.positions[j];
+				} else if (geometry.positions[j] > maxX) {
+					maxX = geometry.positions[j];
+				}
+				if (geometry.positions[j + 1] < minY) {
+					minY = geometry.positions[j + 1];
+				} else if (geometry.positions[j + 1] > maxY) {
+					maxY = geometry.positions[j + 1];
+				}
+				if (geometry.positions[j + 2] < minZ) {
+					minZ = geometry.positions[j + 2];
+				} else if (geometry.positions[j + 2] > maxZ) {
+					maxZ = geometry.positions[j + 2];
+				}
+				cluster[i++] = geometry.positions[j];
+				cluster[i++] = geometry.positions[j + 1];
+				cluster[i++] = geometry.positions[j + 2];
+				cluster[i++] = geometry.normals[j];
+				cluster[i++] = geometry.normals[j + 1];
+				cluster[i++] = geometry.normals[j + 2];
+				cluster[i++] = geometry.tangents[j++];
+				cluster[i++] = geometry.tangents[j++];
+				cluster[i++] = geometry.tangents[j++];
+				cluster[i++] = geometry.uvs[k++];
+				cluster[i++] = geometry.uvs[k++];
+			}
+			var bounds = new h3d.col.Bounds();
+			bounds.xMin = minX;
+			bounds.yMin = minY;
+			bounds.zMin = minZ;
+			bounds.xMax = maxX;
+			bounds.yMax = maxY;
+			bounds.zMax = maxZ;
+			primitive = new h3d.prim.RawPrimitive({
+				vbuf: cluster,
+				ibuf: cast geometry.indices,
+				format: format,
+				bounds: bounds
+			});
 			primitiveCache.set(tag, primitive);
 		}
 		return primitive;
@@ -123,28 +177,24 @@ class SolidDefinition implements Definition {
 		var collider = colliderCache.get(tag);
 		if (collider == null) {
 			var geometry = geometries.get(tag);
-			if (geometry == null || geometry.vertices.length == 0) {
+			if (geometry == null || geometry.positions.length == 0) {
 				return collider;
 			}
-			var primitive = primitiveCache.get(tag);
-			if (primitive == null) {
-				var indices:Array<Int> = [];
-				var vertices:Array<hxd.impl.Float32> = [];
-				vertices.resize(geometry.vertices.length * 3);
-				indices.resize(geometry.indices.length);
-				for (i in 0...geometry.indices.length) {
-					indices[i] = geometry.indices[i];
-				}
-				var i = 0;
-				for (v in geometry.vertices) {
-					vertices[i++] = v.x;
-					vertices[i++] = v.y;
-					vertices[i++] = v.z;
-				}
-				collider = new h3d.col.Polygon();
-				collider.addBuffers(cast vertices, cast indices);
-			} else {
-				collider = cast primitive.getCollider();
+			collider = new h3d.col.Polygon();
+			@:privateAccess for (i in 0...Std.int(geometry.indices.length / 3)) {
+				var k = i * 3;
+				var t = new h3d.col.Polygon.TriPlane(collider.oriented);
+
+				var i0 = geometry.indices[k] * 3;
+				var i1 = geometry.indices[k + 1] * 3;
+				var i2 = geometry.indices[k + 2] * 3;
+
+				t.init(new h3d.Vector(geometry.positions[i0], geometry.positions[i0 + 1], geometry.positions[i0 + 2]),
+					new h3d.Vector(geometry.positions[i1], geometry.positions[i1 + 1], geometry.positions[i1 + 2]),
+					new h3d.Vector(geometry.positions[i2], geometry.positions[i2 + 1], geometry.positions[i2 + 2]));
+
+				t.next = collider.triPlanes;
+				collider.triPlanes = t;
 			}
 			colliderCache.set(tag, collider);
 		}
